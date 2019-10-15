6fa38ef05de0e944b7e046eff9f333786f35b116
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2019-10-15 22:26:23 +0530

    bugs

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,92 +1,90 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 module.exports.register = async (req, res) => {
-  let newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    contact: req.body.contact,
-    password: req.body.password,
-    role: req.body.role
-  });
-  User.addUser(newUser, (err, user) => {
-    if (err) {
-      let message = "";
-      if (err.errors.email)
-        message += "Email already exists.";
-      return res.json({
-        success: false,
-        email: message
-      });
+  let {
+    name,
+    email,
+    contact,
+    password,
+    role
+  } = req.body;
+  if (!name || !email || !contact || !password || !role) {
+    return res.status(400).json({ message: 'All fields are mandatory!!' });
+  }
+  let emailRegex = /^\S+@\S+\.\S+/,
+    phoneRegex = /(^[6-9]{1}[0-9]{9}$)/,
+    passwordRegex = /^[\S]{8,}/;
+  if (emailRegex.test(email)) {
+    if (passwordRegex.test(String(password))) {
+      if (phoneRegex.test(Number(contact))) {
+        let user = await User.findOne({ email });
+        if (user) {
+          return res.status(400).json({ message: 'User already registered!!' });
+        } else {
+          let newUser = {
+            name, email, password, role, contact
+          };
+          const salt = await bcrypt.genSalt(10);
+          newUser.password = await bcrypt.hash(newUser.password, salt);
+          user = await User.create(newUser);
+          return res.status(200).json({
+            success: true,
+            message: "Registeration Successful!"
+          });
+        }
+      } else {
+        res.status(400).json({
+          message: 'Contact number not valid!!'
+        });
+      }
     } else {
-      message = "";
-      message1 = "";
-      message2 = "";
-      var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-      if (emailRegex.test(newUser.email) == false)
-        message += "Email is not valid.";
-      if (req.body.password.length < 8)
-        message1 += "Password must have atleast 8 characters";
-      var isnum = /^\d+$/.test(newUser.contact);
-      if (newUser.contact.length < 10 || newUser.contact.length > 10 || isnum == false)
-        message2 += "Contact No. should be of 10 digits";
-      if (message != "" || message1 != "" || message2 != "")
-        return res.json({
-          success: false,
-          email: message,
-          password: message1,
-          contact: message2
-        }).status(400);
-      else
-        return res.json({
-          success: true,
-          message: "Registeration Successful!"
-        }).status(200);
+      res.status(400).json({
+        message: 'Password must be atleast 8 characters long!!'
+      });
     }
-  });
+  } else {
+    res.status(400).json({ message: 'EmailID is not valid!!' });
+  }
 }
-
-module.exports.login = (req, res) => {
+module.exports.login = async (req, res) => {
   let {
     email,
     password
   } = req.body;
-  let role = req.originalUrl.split('/')[2];
-  User.getUserByEmail(email, role, (err, user) => {
-    if (err) throw err;
-    if (!user) {
-      return res.json({
-        success: false,
-        message: "User not found!"
-      }).status(400);
-    }
-
-    User.comparePassword(password, user.password, (err, isMatch) => {
-      if (err) throw err;
-      if (isMatch) {
-        const token = jwt.sign({
-          type: "user",
-          data: {
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            contact: user.contact,
-            role: user.role
-          }
-        }, config.secret, {
-          expiresIn: 604800 // for 1 week time in milliseconds
-        });
-        return res.json({
-          success: true,
-          token: "JWT " + token
-        }).status(200);
-      } else {
-        return res.json({
-          success: true,
-          message: "Wrong Password."
-        }).status(401);
+  let user = await User.findOne({ email });
+  if (!user) {
+    return res.json({
+      success: false,
+      message: "User not found!"
+    }).status(400);
+  }
+  debugger
+  let isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    const token = jwt.sign({
+      type: "user",
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        contact: user.contact,
+        role: user.role
       }
+    }, process.env.secret, {
+      expiresIn: 604800 // for 1 week time in milliseconds
     });
-  });
+    return res.json({
+      success: true,
+      token: "JWT " + token
+    }).status(200);
+  } else {
+    return res.json({
+      success: true,
+      message: "Wrong Password."
+    }).status(401);
+  }
 }
-
 module.exports.profile = (req, res) => {
   // console.log(req.user);
   return res.json(
